891e2dd7e523d1c34bf8a2b18440183a8c1f96eb
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-05-17 00:18:00 +0700

    Initial commit

---
+++ src/TripDes/BASE64Encoder.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TripDes;
+
+/**
+ *
+ * @author Hieu Nghia Le
+ */
+class BASE64Encoder {
+    
+}
